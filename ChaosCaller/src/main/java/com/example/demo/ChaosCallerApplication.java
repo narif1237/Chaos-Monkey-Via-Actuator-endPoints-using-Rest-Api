4999aee3f2a743b3b74ab1f81eff3e4d574155ac
narif1237 <narif1237@gmail.com>
2018-12-03 06:15:42 +0530

    added an app to induce choas in other application

---
+++ ChaosCaller/src/main/java/com/example/demo/ChaosCallerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosCallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaosCallerApplication.class, args);
+	}
+}
---
+++ ChaosCaller/src/main/java/com/example/demo/controller/ChaosController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+public class ChaosController {
+
+	@GetMapping("/chaosStart")
+	public String startChaos() {
+	RestTemplate restTemplate=new RestTemplate();
+		String s = null;
+		HttpHeaders headers=new HttpHeaders();
+		JSONObject body=new JSONObject();
+		body.put("exceptionsActive", false);
+		HttpEntity<JSONObject> request=new HttpEntity<>(body);
+		ResponseEntity<String> enableResult=restTemplate.exchange("http://localhost:8080/actuator/chaosmonkey/enable", HttpMethod.POST, null, String.class);
+		return enableResult.getBody();
+	}
+	@GetMapping("/chaosExceptionAssault")
+	public String startExceptionChaos() {
+	RestTemplate restTemplate=new RestTemplate();
+		String s = null;
+		HttpHeaders headers=new HttpHeaders();
+		String body="{\r\n" + 
+				"\"level\": 5,\r\n" + 
+				"\"latencyRangeStart\": 2000,\r\n" + 
+				"\"latencyRangeEnd\": 5000,\r\n" + 
+				"\"latencyActive\": false,\r\n" + 
+				"\"exceptionsActive\": true,\r\n" + 
+				"\"killApplicationActive\": false,\r\n" + 
+				"\"restartApplicationActive\": false\r\n" + 
+				"}";
+
+headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		HttpEntity<String> request=new HttpEntity<>(body,headers);
+		ResponseEntity<String> enableResult=restTemplate.exchange("http://localhost:8080/actuator/chaosmonkey/assaults", HttpMethod.POST, request, String.class);
+		return enableResult.getBody();
+	}
+
+}
