c2de231c87f7b96379648c960f9b75bece3f6d12
narif1237 <narif1237@gmail.com>
2018-12-03 06:38:04 +0530

    sonar fixes

--- Acutator/src/main/java/com/example/demo/controller/EndpointsForChaosChecking.java
+++ Acutator/src/main/java/com/example/demo/controller/EndpointsForChaosChecking.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class control {
+public class EndpointsForChaosChecking {
 	 @RequestMapping("/greeting")
 	    public String greeting() {
 	        return "hello";
